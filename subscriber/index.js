06f903340cdd989fc7be0a3be86210d16f93a871
Nikita Pavlov <npavlov1337@gmail.com>
2023-03-06 13:55:11 +0300

    initial commit

---
+++ subscriber/index.js
@@ -0,0 +1,30 @@
+const kafka = require('kafka-node');
+const client = new kafka.KafkaClient({
+	kafkaHost:
+		process.env.ENVIRONMENT === 'local'
+		? process.env.INTERNAL_KAFKA_ADDR
+		: process.env.EXTERNAL_KAFKA_ADDR,
+});
+const Consumer = kafka.Consumer;
+
+const consumer  = new Consumer(
+	client,
+	[
+		{
+			topic: process.env.TOPIC,
+			partition: 0,
+		},
+	],
+	{
+		autoComit: false,
+	
+	},
+);
+
+consumer.on('message', message => {
+	console.log(message);
+});
+
+consumer.on('error', err => {
+	console.log(err);
+});
